dde20bf7a31f349044384d65c30b218bcc684373
tiagoantunes13 <tiagoantunes9991@gmail.com>
2022-07-25 23:02:03 +0100

    TURRBO PART 1

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,23 @@
+class StocksController < ApplicationController
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock == nil
+        flash[:alert] = "Please enter a valid symbol"
+        redirect_to my_portfolio_path
+      else
+        #render "users/my_portfolio"
+
+        respond_to do |format|
+          format.turbo_stream { render turbo_stream: turbo_stream.prepend("idteste", partial: "users/result") }
+          format.html { render "users/my_portfolio"} ## Specify the format in which you are rendering "new" page
+
+        end
+        #render 'users/my_portfolio'
+      end
+    else
+      flash[:alert] = "Please enter a symbol to search"
+      redirect_to my_portfolio_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "welcome#index"
   devise_for :users
+  get "my_portfolio", to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
 end
