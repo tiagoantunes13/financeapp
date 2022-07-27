8c80360427937590eb62efbac84c5d57a1de0267
tiagoantunes13 <tiagoantunes9991@gmail.com>
2022-07-27 04:27:30 +0100

    TURRBO WORKING

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,22 +1,40 @@
 class StocksController < ApplicationController
   def search
+    p params
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock == nil
-        flash[:alert] = "Please enter a valid symbol"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash[:alert] = "Enter a valid Stock"
+          format.turbo_stream do
+            render turbo_stream: [
+              turbo_stream.update(:messagess, partial: "layouts/messages"),
+              turbo_stream.update(:formUpdate, partial: "stocks/searchform", locals: {stock: "asdsda"})
+            ]
+          end
+          #format.turbo_stream { render turbo_stream: turbo_stream.update(:idteste, partial: "users/result")}
+          #format.html { render root_path, alert: "Please enter a valid symbolaa"} ## Specify the format in which you are rendering "new" page
+        end
       else
         #render "users/my_portfolio"
 
         respond_to do |format|
-          format.turbo_stream { render turbo_stream: turbo_stream.update(:idteste, partial: "users/result") }
+          format.turbo_stream do
+            render turbo_stream: [
+              turbo_stream.prepend(:idteste, partial: "users/result"),
+              turbo_stream.update(:formUpdate, partial: "stocks/searchform", locals: {stock: "Asdasd"})
+            ]
           #format.html { render 'users/my_portfolio'} ## Specify the format in which you are rendering "new" page
-        end
+          end
+          end
         #render 'users/my_portfolio'
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash[:alert] = "Enter a Stock"
+        format.turbo_stream { render turbo_stream: turbo_stream.update(:messagess, partial: "layouts/messages")}
+        #format.html { render 'users/my_portfolio'} ## Specify the format in which you are rendering "new" page
+      end
     end
   end
 end
